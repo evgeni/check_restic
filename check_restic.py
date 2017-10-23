029084944422eb4db6a95c415e7f0611035730ab
Pieter Lexis <pieter@plexis.eu>
2017-10-23 19:27:55 +0200

    check_restic.py: Big rework
     * Support multi-path when invoked without --path
     * Add --sudo for running with sudo
     * Add --restic-bin for specifying the name of the binary
     * Add more error handling when running restic
     * Add debug logging
     * Add a Summary class for prettier output
     * Move arguments into the class for possible later filtering

--- check_restic.py
+++ check_restic.py
@@ -13,30 +13,98 @@ _log = logging.getLogger('nagiosplugin')
 
 class Restic(nagiosplugin.Resource):
 
-    restic_cmd = 'restic'
-
-    def __init__(self, restic_args=[]):
-        self.restic_args = restic_args
+    def __init__(self, restic_bin='restic', host=None, path=None, repo=None, password_file=None, sudo=False):
+        self.restic_bin = restic_bin
+        self.host = host
+        self.path = path
+        self.repo = repo
+        self.password_file = password_file
+        self.sudo = sudo
 
     def probe(self):
-        cmd = [self.restic_cmd, 'snapshots', '--json', '--no-lock'] + self.restic_args
+        cmd = []
+
+        if self.sudo:
+            cmd = ['sudo'] + cmd
+
+        cmd.extend([self.restic_bin, '--json', '--no-lock', 'snapshots'])
+
+        if self.host:
+            cmd.extend(['--host', self.host])
+        if self.path:
+            cmd.extend(['--path', self.path])
+        if self.repo:
+            cmd.extend(['--repo', self.repo])
+        if self.password_file:
+            cmd.extend(['--password-file', self.password_file])
+
+        _log.info('Using command: %s' % ' '.join(cmd))
+
         try:
-            restic_result = subprocess.check_output(cmd)
-        except:
-            raise nagiosplugin.CheckError('Failed to run %s' % cmd)
-        snapshots = json.loads(restic_result)
+            restic_result = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+        except subprocess.CalledProcessError as e:
+            raise nagiosplugin.CheckError('Failed to run %s: %s' % (' '.join(cmd), e.output.decode()))
+        except FileNotFoundError as e:
+            raise nagiosplugin.CheckError('Failed to run %s: %s' % (' '.join(cmd), e))
+        _log.debug('Got output: %s' % restic_result)
+
+        try:
+            snapshots = json.loads(restic_result)
+        except json.decoder.JSONDecodeError as e:
+            raise nagiosplugin.CheckError('Unable to parse restic output: %s' % e)
+        _log.debug('Output decoded to: %s' % snapshots)
+
         if not snapshots:
             raise nagiosplugin.CheckError('Could not find snapshots')
-        snapshots.sort(key=lambda snapshot: dateutil.parser.parse(snapshot['time']))
-        last_snapshot = dateutil.parser.parse(snapshots[-1]['time']).replace(tzinfo=None)
-        last_snapshot_age = datetime.datetime.utcnow() - last_snapshot
-        last_snapshot_age = last_snapshot_age.total_seconds() / (60*60)
-        return nagiosplugin.Metric('last_snapshot_age', last_snapshot_age, uom='h')
+        snapshots.sort(key=lambda snapshot: dateutil.parser.parse(snapshot['time']),
+                       reverse=True)
+        last_snapshots = {}
+
+        while True:
+            try:
+                e = next(e for e in snapshots if '_'.join(e['paths']) not in last_snapshots.keys())
+                last_snapshots['_'.join(e['paths'])] = e
+            except StopIteration:
+                break
+
+        for path, snapshot in last_snapshots.items():
+            snapshot_age = datetime.datetime.now(None) - dateutil.parser.parse(snapshot['time']).replace(tzinfo=None)
+            snapshot_age = snapshot_age.total_seconds() / (60*60)
+            yield nagiosplugin.Metric(path, snapshot_age, uom='h', context='last_snapshot_age')
+
+
+class ResticSummary(nagiosplugin.Summary):
+    def ok(self, results):
+        """
+        Show all results in the output
+
+        :param results:
+        :return:
+        """
+        ret = ['%s is %.2f hours old' % (r.metric.name, r.metric.value) for r in results]
+        return 'Snapshot %s' % ', '.join(ret)
+
+    def problem(self, results):
+        """
+        Show only the results that have crossed the threshold
+
+        :param results:
+        :return:
+        """
+        if results.results[0].state == nagiosplugin.Unknown:
+            return results.results[0].hint
+
+        ret = ['%s is %2f hours old' % (r.metric.name, r.metric.value) for r in results if r.state != nagiosplugin.Ok]
+        return 'Snapshot %s' % ', '.join(ret)
 
 
 @nagiosplugin.guarded
 def main():
     argp = argparse.ArgumentParser(description=__doc__)
+    argp.add_argument('--sudo', action='store_true',
+                      help='Use "sudo" when invoking restic (default: %(default)s)')
+    argp.add_argument('--restic-bin', type=str, metavar='RESTIC-BIN', default='restic',
+                      help='Path to the restic binary, or the name of restic in $PATH (default: %(default)s)')
     argp.add_argument('-w', '--warning', metavar='HOURS', type=int, default=24,
                       help='Snapshots older than HOURS are WARNING (default: %(default)s)')
     argp.add_argument('-c', '--critical', metavar='HOURS', type=int, default=48,
@@ -53,19 +121,11 @@ def main():
                       help='increase output verbosity (use up to 3 times)')
     args = argp.parse_args()
 
-    restic_args = []
-    if args.host:
-        restic_args.extend(['--host', args.host])
-    if args.path:
-        restic_args.extend(['--path', args.path])
-    if args.repo:
-        restic_args.extend(['--repo', args.repo])
-    if args.password_file:
-        restic_args.extend(['--password-file', args.password_file])
-
     check = nagiosplugin.Check(
-        Restic(restic_args),
+        Restic(restic_bin=args.restic_bin, host=args.host, path=args.path, repo=args.repo,
+               password_file=args.password_file, sudo=args.sudo),
         nagiosplugin.ScalarContext('last_snapshot_age', args.warning, args.critical),
+        ResticSummary(),
         )
 
     check.main(verbose=args.verbose)
